46ea993f227d2d4435531647686efff59fd95d06
Robert Rahardja <robertrahardja@Roberts-iMac.local>
2020-10-13 10:23:52 +0800

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,20 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen';
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+
 const App = () => {
   return (
     <Router>
       <Header />
       <main>
         <Container className='py-3'>
+          <Route path='/login' component={LoginScreen} />
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={ProductScreen}  />
-          <Route path='/cart/:id?' component={CartScreen}  />
-
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
-
       </main>
       <Footer />
     </Router>
